60fca5068e85d19f31d8f49dc6bba731af3951c6
zongzi <545670755@qq.com>
2021-06-16 16:43:29 +0800

    prepare.sh

--- prepare.sh
+++ prepare.sh
@@ -1,1 +1,1 @@
-git flow feature start test2 &&  git push origin feature/test2 && git flow feature start test3 &&  git push origin feature/test3 && git checkout main &&  git branch -D feature/test2 &&  git branch -D feature/test3 && git branch -D `git branch -a |  grep "\* release"  | awk -F '/' '{print $2}'`
+git flow feature start test2 &&  git push origin feature/test2 && git flow feature start test3 &&  git push origin feature/test3 && git checkout main &&  git branch -D feature/test2 &&  git branch -D feature/test3 
