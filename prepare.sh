9d9f29bb88302886fddbdd0008dabab702c956de
zongzi <545670755@qq.com>
2021-06-16 16:09:36 +0800

    prepare.sh

---
+++ prepare.sh
@@ -0,0 +1,8 @@
+git flow feature start test2
+git push origin feature/test2
+git flow feature start test3
+git push origin feature/test3
+git checkout main
+git branch -D feature/test2
+git branch -D feature/test3
+
